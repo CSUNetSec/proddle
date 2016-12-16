c56d84427a4fd600f1502f088a692b6d2010af95
Dan Rammer <hamersaw@bushpath.com>
2016-12-16 12:40:01 -0700

    added first module, a simple http-get request.

---
+++ modules/core/http-get.py
@@ -0,0 +1,35 @@
+import pycurl
+import sys
+
+try:
+    from io import BytesIO
+except ImportError:
+    from StringIO import StringIO as BytesIO
+
+#create curl request
+buffer = BytesIO()
+c = pycurl.Curl()
+c.setopt(c.URL, sys.argv[1])
+c.setopt(c.WRITEDATA, buffer)
+c.setopt(c.FOLLOWLOCATION, True)
+
+#execute request
+try:
+    c.perform()
+
+    #print body
+    body = buffer.getvalue()
+    #print(body.decode('iso-8859-1'))
+
+    #print information
+    print('{', end='')
+    print('"HTTPStatusCode":%d,' % c.getinfo(c.RESPONSE_CODE), end='')
+    print('"ApplicationLayerLatency":%f,' % c.getinfo(c.TOTAL_TIME), end='')
+    print('"PrimaryIP":"%s",' % c.getinfo(c.PRIMARY_IP), end='')
+    print('"RedirectCount":%d' % c.getinfo(c.REDIRECT_COUNT), end='')
+    print('}', flush=True)
+except:
+    print('error')
+
+#close curl handle
+c.close()
