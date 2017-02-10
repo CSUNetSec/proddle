204415d3e6ec7d3555d6bbf5413d5cd0464b7b38
Daniel Rammer <rammerd@proddle.netsec.colostate.edu>
2017-02-10 06:48:34 -0700

    updated http-get measurement and add-top-alexa script with some basic changes.

--- docs/measurements/http-get.py
+++ docs/measurements/http-get.py
@@ -6,48 +6,41 @@ try:
 except ImportError:
     from StringIO import StringIO as BytesIO
 
-def main():
-    #create curl request
-    buffer = BytesIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, sys.argv[1])
-    c.setopt(c.WRITEDATA, buffer)
-    c.setopt(c.FOLLOWLOCATION, True)
-    c.setopt(c.TIMEOUT, 120)
+#create curl request
+buffer = BytesIO()
+c = pycurl.Curl()
+c.setopt(c.URL, sys.argv[1])
+c.setopt(c.WRITEDATA, buffer)
+c.setopt(c.FOLLOWLOCATION, True)
+c.setopt(c.TIMEOUT, 120)
 
-    #execute request
-    try:
-        c.perform()
+#execute request
+try:
+    c.perform()
 
-        #print body
-        body = buffer.getvalue()
-        #print(body.decode('iso-8859-1'))
+    #print body
+    body = buffer.getvalue()
+    #print(body.decode('iso-8859-1'))
 
-        #print information
-        print('{', end='')
-        print('"error":false,', end='')
-        print('"effective_url":"%s",' % c.getinfo(c.EFFECTIVE_URL), end='')
-        print('"domain_ip":"%s",' % c.getinfo(c.PRIMARY_IP), end='')
-        print('"domain_port":%d,' % c.getinfo(c.PRIMARY_PORT), end='')
-        #print('"local_ip":"%s",' % c.getinfo(c.LOCAL_IP), end='')
-        print('"application_layer_latency":%f,' % c.getinfo(c.TOTAL_TIME), end='')
-        print('"redirect_count":%d,' % c.getinfo(c.REDIRECT_COUNT), end='')
-        print('"http_status_code":%d,' % c.getinfo(c.RESPONSE_CODE), end='')
-        print('"request_size":%d,' % c.getinfo(c.REQUEST_SIZE), end='')
-        print('"content_size":%d' % len(body), end='')
-        print('}', end='', flush=True)
+    #print information
+    print('{', end='')
+    print('"error":false,', end='')
+    print('"effective_url":"%s",' % c.getinfo(c.EFFECTIVE_URL), end='')
+    print('"domain_ip":"%s",' % c.getinfo(c.PRIMARY_IP), end='')
+    print('"domain_port":%d,' % c.getinfo(c.PRIMARY_PORT), end='')
+    #print('"local_ip":"%s",' % c.getinfo(c.LOCAL_IP), end='')
+    print('"application_layer_latency":%f,' % c.getinfo(c.TOTAL_TIME), end='')
+    print('"redirect_count":%d,' % c.getinfo(c.REDIRECT_COUNT), end='')
+    print('"http_status_code":%d,' % c.getinfo(c.RESPONSE_CODE), end='')
+    print('"request_size":%d,' % c.getinfo(c.REQUEST_SIZE), end='')
+    print('"content_size":%d' % len(body), end='')
+    print('}', end='', flush=True)
+except:
+    e = sys.exc_info()[0] #catch all errors
+    print('{', end='')
+    print('"error":true', end='')
+    print(',"error_message":"%s"' % c.errstr(), end='')
+    print('}', end='', flush=True)
 
-        return
-    except:
-        e = sys.exc_info()[0] #catch all errors
-        print('{', end='')
-        print('"error":true', end='')
-        print(',"error_message":"%s"' % c.errstr(), end='')
-        print('}', end='', flush=True)
-
-    #close curl handle
-    c.close()
-
-
-if __name__ == '__main__':
-    main()
+#close curl handle
+c.close()
--- docs/scripts/add-top-alexa.sh
+++ docs/scripts/add-top-alexa.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 COUNT USERNAME PASSWORD"
+    exit 1
+fi
+
 TMP_DIR="/tmp"
 PREFIXES=(
-    "https://www."
+#    "https://www."
     "http://www."
 )
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 COUNT"
-    exit 1
-fi
+USERNAME=$2
+PASSWORD=$3
+SSL_CA_CERT="/etc/ssl/mongodb/cacert.pem"
+SSL_CERT="/etc/ssl/mongodb/proddle.crt"
+SSL_KEY="/etc/ssl/mongodb/proddle.key"
+MONGO_HOST="mongo1.netsec.colostate.edu"
 
 #download alexa top 1 million domains
 wget -O $TMP_DIR/top-1m.csv.zip http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
@@ -22,9 +29,9 @@ do
     DOMAIN=`echo $LINE | cut -f 2 -d ','`
 
     for PREFIX in "${PREFIXES[@]}"; do
-        curl -L $PREFIX$DOMAIN >> /dev/null
+        curl -L $PREFIX$DOMAIN -m 30 >> /dev/null
         if [ $? -eq 0 ]; then
-            ./../../yogi/target/debug/yogi operation add http-get $DOMAIN $PREFIX$DOMAIN -t core -t http
+            ./../../yogi/target/debug/yogi -c $SSL_CA_CERT -e $SSL_CERT -k $SSL_KEY -u $USERNAME -p $PASSWORD -I $MONGO_HOST operation add http-get $DOMAIN $PREFIX$DOMAIN -t core -t http
             echo "SUCCESS $DOMAIN"
 
             #increment counter
