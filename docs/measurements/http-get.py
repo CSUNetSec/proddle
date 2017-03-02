72237e6a2632466e62a448b9ee4fbd8d02bde9e4
Daniel Rammer <hamersaw@bushpath.com>
2017-03-02 14:08:37 -0700

    updated http-get measurement and domains for operations for better results.

--- docs/measurements/http-get.py
+++ docs/measurements/http-get.py
@@ -16,9 +16,21 @@ c.setopt(c.TIMEOUT, 120)
 c.setopt(c.USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/55.0.2883.87 Chrome/55.0.2883.87 Safari/537.36')
 
 #execute request
+failure=0
 try:
     c.perform()
+except:
+    #e = sys.exc_info()[0] #catch all errors
+    if c.errstr() != "GnuTLS recv error (-110): The TLS connection was non-properly terminated.":
+        failure=1
 
+#parse for failure
+if failure:
+    print('{', end='')
+    print('"error":true', end='')
+    print(',"error_message":"%s"' % c.errstr(), end='')
+    print('}', end='', flush=True)
+else:
     #print body
     body = buffer.getvalue()
     #print(body.decode('iso-8859-1'))
@@ -29,18 +41,11 @@ try:
     print('"effective_url":"%s",' % c.getinfo(c.EFFECTIVE_URL), end='')
     print('"domain_ip":"%s",' % c.getinfo(c.PRIMARY_IP), end='')
     print('"domain_port":%d,' % c.getinfo(c.PRIMARY_PORT), end='')
-    #print('"local_ip":"%s",' % c.getinfo(c.LOCAL_IP), end='')
     print('"application_layer_latency":%f,' % c.getinfo(c.TOTAL_TIME), end='')
     print('"redirect_count":%d,' % c.getinfo(c.REDIRECT_COUNT), end='')
     print('"http_status_code":%d,' % c.getinfo(c.RESPONSE_CODE), end='')
     print('"request_size":%d,' % c.getinfo(c.REQUEST_SIZE), end='')
     print('"content_size":%d' % len(body), end='')
-    print('}', end='', flush=True)
-except:
-    e = sys.exc_info()[0] #catch all errors
-    print('{', end='')
-    print('"error":true', end='')
-    print(',"error_message":"%s"' % c.errstr(), end='')
     print('}', end='', flush=True)
 
 #close curl handle
